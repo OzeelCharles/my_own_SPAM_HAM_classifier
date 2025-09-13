392634ab7eab4be2da1b91d24a3e5832b31cdede
OzeelCharles <charles.ozeel@ensai-eleve.fr>
2025-09-14 00:11:05 +0200

    pipeline d'embedding

---
+++ src/embedding.py
@@ -0,0 +1,12 @@
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+
+data = pd.read_csv("SMS_HAM_SPAM.csv")
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+embeddings = model.encode(data["Text"].tolist())
+
+data["embeddings"] = embeddings
+
+data.to_csv("SMS_HAM_SPAM_with_embeddings.csv", index=False)
